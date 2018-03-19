a44999b84243bcebbae4a453ecb043f5a625e892
soubhagyarout <soubhagya.rout@gmaail.com>
2018-03-20 03:30:03 +0530

    7th commit

--- Batch.bat
+++ Batch.bat
@@ -1,27 +1,20 @@
-
-
-
-
+echo off
+:: this is Soiubhagya
 echo "Radhe Shyam"
 
-echo "Hello World INDIA !!!"
+echo Hello World INDIA !!!
 
-echo "Hello World INDIA !!!"
+echo Hello World INDIA !!!
+echo Hello World INDIA !!!
+echo Hello World INDIA !!!
+echo Hello World INDIA !!!
+echo Hello World INDIA !!!
 
-echo "Hello World INDIA !!!"
+echo Hello World INDIA !!!
+echo Hello World INDIA !!!
+echo Hello World INDIA !!!
 
-echo "Hello World INDIA !!!"
 
-echo "Hello World INDIA !!!"
-
-echo "Hello World INDIA !!!"
-
-echo "Hello World INDIA !!!"
-
-echo "Hello World INDIA !!!"
-
-echo "Hello World INDIA !!!"
-
-echo "Hello World INDIA !!!"
 
 echo Jai Sairam
+pause
